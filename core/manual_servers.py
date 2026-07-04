e986ae4f718055f2e92cdf5a0af17956ec5dac39
Tomek Wilczyński <tjomek@gmail.com>
2026-07-04 17:18:55 +0200

    v0.4 add manual server storage

---
+++ core/manual_servers.py
@@ -0,0 +1,94 @@
+import json
+from pathlib import Path
+
+
+CONFIG_DIR = Path.home() / ".config" / "smb-mount-wizard"
+
+CONFIG_FILE = CONFIG_DIR / "manual_servers.json"
+
+
+def ensure():
+
+    CONFIG_DIR.mkdir(
+
+        parents=True,
+
+        exist_ok=True
+
+    )
+
+    if not CONFIG_FILE.exists():
+
+        CONFIG_FILE.write_text("[]")
+
+
+def get_servers():
+
+    ensure()
+
+    try:
+
+        return json.loads(
+
+            CONFIG_FILE.read_text()
+
+        )
+
+    except Exception:
+
+        return []
+
+
+def save_servers(servers):
+
+    ensure()
+
+    CONFIG_FILE.write_text(
+
+        json.dumps(
+
+            sorted(
+
+                list(set(servers))
+
+            ),
+
+            indent=4
+
+        )
+
+    )
+
+
+def add_server(server):
+
+    servers = get_servers()
+
+    servers.append(server)
+
+    save_servers(
+
+        servers
+
+    )
+
+
+def remove_server(server):
+
+    servers = get_servers()
+
+    servers = [
+
+        s
+
+        for s in servers
+
+        if s != server
+
+    ]
+
+    save_servers(
+
+        servers
+
+    )
